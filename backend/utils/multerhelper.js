bcb97489d994715989be246b00d50709e355752a
Ayush Chakraborty <ayushchakraborty.cse19@chitkarauniversity.edu.in>
2026-02-18 20:33:36 +0530

    Project initialization, installed required dependencies and created multer helper function

---
+++ backend/utils/multerhelper.js
@@ -0,0 +1,26 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = [".glb", ".gltf"];
+  const ext = path.extname(file.originalname).toLowerCase();
+
+  if (allowedTypes.includes(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only .glb and .gltf files are allowed"));
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+module.exports = upload;
